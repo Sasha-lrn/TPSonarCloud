1cf07e9fc7eb2a59b6780aaae08b71f6725df030
sashad <sashademendonca@gmail.com>
2026-04-24 10:44:12 +0200

    100% des test

--- tests/BankAccountTest.php
+++ tests/BankAccountTest.php
@@ -24,5 +24,22 @@ class BankAccountTest extends TestCase
         $this->assertEquals(100, $compte->getBalance());       
     }
 
+    public function testComputeFee(): void
+    {
+        $compte = new BankAccount("Alice", 100);
+        $compte->computeFee(2);
+        $this->assertEquals(200, $compte->computeFee(2));       
+    }
+
+    public function testTransfer(): void
+    {
+        $compte = new BankAccount("Alice", 100);
+        $compte2 = new BankAccount("Robin", 100);
+        $compte->transfer($compte2, 50);
+
+        $this->assertEquals(50, $compte->getBalance());
+        $this->assertEquals(150, $compte2->getBalance());       
+    }
+
     
 }
